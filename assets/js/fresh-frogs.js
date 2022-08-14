5e80985b05e2aa8f07a28843d50ef52419101d37
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-08-14 18:16:50 -0500

    Update fresh-frogs.js

--- assets/js/fresh-frogs.js
+++ assets/js/fresh-frogs.js
@@ -421,7 +421,13 @@ async function connect() {
       //
 
       let incomingTokenTransferEvents = await controller.getPastEvents('Stake', { filter: {'from': userAcc}, fromBlock: 0, toBlock: 'latest'})
-        incomingTokenTransferEvents.forEach( (event) => console.log('Stake() Event Found!'); console.log(event.returnValues));
+        incomingTokenTransferEvents.forEach( (event) => console.log(event.returnValues))
+        .catch(e => {
+
+          console.log(e.message);
+      
+        })
+        
 
       //
       frog_token.onclick = function() { 
