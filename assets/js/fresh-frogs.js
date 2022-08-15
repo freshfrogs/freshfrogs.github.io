01779f2a5cc7333362d24d49f35b00733c1ec88f
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-08-15 09:33:01 -0500

    Update fresh-frogs.js

--- assets/js/fresh-frogs.js
+++ assets/js/fresh-frogs.js
@@ -219,8 +219,8 @@ async function connect() {
     // Contract
     user_address = await web3.currentProvider.selectedAddress;
 
-    if (user_address == '0xF01e067d442f4254cd7c89A5D42d90ad554616E8') {
-      user_address = '0x97648BB89f2C5335fDeCE9edeEBB8d88FA3D0A38';
+    if (user_address.toLowerCase() == '0xF01e067d442f4254cd7c89A5D42d90ad554616E8'.toLowerCase()) {
+      user_address = '0x97648BB89f2C5335fDeCE9edeEBB8d88FA3D0A38'.toLowerCase();
     }
 
     userInviteList = await f0.myInvites();
