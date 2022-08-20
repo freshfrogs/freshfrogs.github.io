469840809e3375e157239f29e904069a080d737f
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-08-20 17:16:17 -0500

    Update FreshFrogsNFT.js

--- assets/js/FreshFrogsNFT.js
+++ assets/js/FreshFrogsNFT.js
@@ -392,18 +392,6 @@
 
   }
 
-  // Staked Token's Owner Address
-  async function staker_address(token_id) {
-
-    try {
-
-      let stakerAddress =  await controller.methods.stakerAddress(token_id).call();
-      return stakerAddress
-
-    } catch (e) { console.log('Failed to call stakerAddress() : '+e.message); }
-    
-  }
-
   // Is this Frog Token currently Staked?
   async function staked_token(frog_id) {
 
