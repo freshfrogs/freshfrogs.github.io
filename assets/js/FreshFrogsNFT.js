f11e0fe1325eb80c5db6958fcd717bb23132071a
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-08-20 22:00:34 -0500

    Update FreshFrogsNFT.js

--- assets/js/FreshFrogsNFT.js
+++ assets/js/FreshFrogsNFT.js
@@ -272,6 +272,7 @@
         // Interations of 50
         let pages = parseInt(user_tokens/50) + 1; // Round Pages up by one
         for (var i = 0; i < pages; i++) {
+          console.log('Pages: '+pages)
           // Fetch OpenSea Data
           fetch('https://api.opensea.io/api/v1/assets?owner='+fetch_address+'&order_direction=asc&asset_contract_address=0xBE4Bef8735107db540De269FF82c7dE9ef68C51b&offset='+(i * 50)+'&limit=50&include_orders=false', options)
           .then((tokens) => tokens.json())
@@ -288,7 +289,7 @@
                 } else {
                   render_token(token_id);
                 }
-              } catch (e) {} // Suppress Error
+              } catch (e) {console.log(e.message)} // Suppress Error
             })
           })
           .catch(e => { // OpenSea Error
