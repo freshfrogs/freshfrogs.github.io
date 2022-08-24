25c5d033ab2105bcc0c8b0d268661ff88e5b453b
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-08-24 10:08:21 -0500

    Update FreshFrogsNFT.js

--- assets/js/FreshFrogsNFT.js
+++ assets/js/FreshFrogsNFT.js
@@ -451,7 +451,7 @@
 
       // Insert Owner Element
       var trait_text = document.createElement('div')
-      trait_text.innerHTML = 'Owner: '+truncateAddress(staked)+'<br>'+'Time Staked: '+staked_time_bool+' hours'+'<br>';
+      trait_text.innerHTML = 'Owner: '+truncateAddress(staked)+'<br>'+'Time Staked: '+staked_time_bool+' hours';
       document.getElementById('prop_'+frog_id).appendChild(trait_text);      
 
       // Update Progress Bar
