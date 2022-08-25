a9fdbffa99efe67e9fa3836b8d7d92ed56f9709d
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-08-25 16:35:39 -0500

    Update FreshFrogsNFT.js

--- assets/js/FreshFrogsNFT.js
+++ assets/js/FreshFrogsNFT.js
@@ -339,7 +339,7 @@
     button_left.href = etherscanLink;
     button_left.target = '_blank';
 
-    button_middle.innerHTML = '<strong>Opensea</strong>view on';
+    button_middle.innerHTML = '<strong>Opensea</strong>page';
     button_middle.href = openseaLink
     button_middle.target = '_blank';
 
