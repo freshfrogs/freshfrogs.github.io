529b0889c5f8edbdeb9861bda0bd8b6ec00ff33e
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-08-25 00:58:51 -0500

    Update FreshFrogsNFT.js

--- assets/js/FreshFrogsNFT.js
+++ assets/js/FreshFrogsNFT.js
@@ -526,7 +526,7 @@
   // Shorten Ethereum Address
   function truncateAddress(address) {
     if (!address) { return ""; }
-    return `${address.substr(0, 5)}...${address.substr(
+    return `${address.substr(0, 5)}..${address.substr(
       address.length - 5,
       address.length
     )}`;
