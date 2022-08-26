6013a004a4b41cca6e9fdd6673d88cd3b83d29c3
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-08-25 19:50:14 -0500

    Update FreshFrogsNFT.js

--- assets/js/FreshFrogsNFT.js
+++ assets/js/FreshFrogsNFT.js
@@ -604,7 +604,7 @@
         'Please sign the transaction and wait...<br>Do not leave or refresh the page!'+'<br>'+
         '<br><div style="text-align: left;">'+
           '<strong>(1/2) Approve Staking</strong><br>This is a one time transaction to allow staking, requires a gas fee.<br>'+
-          '<br>While your Frog is staked, you will not be able to sell it on secondary market places. To do this you will have to un-stake your Frog directly from this site. When a Frog is un-staked the staking level will reset to zero.'+
+          '<br><strong>Please Read</strong><br>While your Frog is staked, you will not be able to sell it on secondary market places. To do this you will have to un-stake your Frog directly from this site. When a Frog is un-staked the staking level will reset to zero.'+
         '</div>'
       );
 
