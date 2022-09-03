b26a9f38d404ba8e7d1cfca98159f17c36c3c018
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-09-02 19:44:11 -0500

    Update FreshFrogsNFT.js

--- assets/js/FreshFrogsNFT.js
+++ assets/js/FreshFrogsNFT.js
@@ -538,8 +538,8 @@
       if (staked_time_bool >= 2000) { staked_level = 3; } else if (staked_time_bool >= 1000) { staked_level = 2; } else { staked_level = 1; }
 
       document.getElementById('staked_'+frog_id).innerHTML = 
-        '<div style="color:tomato;" class="frog_level"><b>Level '+staked_level+'</b></div>'+
-        '<b id="progress_'+frog_id+'"></b><div class="myProgress" id="myProgress_'+frog_id+'"><div class="myBar" id="myBar_'+frog_id+'"></div></div>';
+        '<b id="progress_'+frog_id+'"></b><div class="myProgress" id="myProgress_'+frog_id+'"><div class="myBar" id="myBar_'+frog_id+'"></div></div>'+
+        '<div style="color:tomato;" class="frog_level"><b>Level '+staked_level+'</b></div>';
 
       var trait_text = document.createElement('div')
       if (staked_time_bool >= 720) { trait_text.innerHTML = 'Staked: '+parseInt(staked_time_bool/24)+' days 🔥<br>'; } 
