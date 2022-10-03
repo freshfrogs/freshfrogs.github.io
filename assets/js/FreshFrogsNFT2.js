d79d5949a6d90c38182e20ce96392badbff08e93
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-10-03 02:18:49 -0500

    sa

--- assets/js/FreshFrogsNFT2.js
+++ assets/js/FreshFrogsNFT2.js
@@ -488,9 +488,9 @@
         '<div style="text-align: left; margin: 8px; height: 16px;">'+
           '<strong id="frog_'+frog_id+'" class="frog_name">'+frog_name+'</strong><strong id="price_'+frog_id+'" class="frog_price">'+recent_sale+'</strong>'+
         '</div>'+
-        '<div class="frog_imgContainer" id="cont_'+frog_id+'" onclick="display_token('+frog_id+')">'+
+        //'<div class="frog_imgContainer" id="cont_'+frog_id+'" onclick="display_token('+frog_id+')">'+
           '<img src="'+frog_external+'" class="recentMint"/>'+
-        '</div>'+
+        //'</div>'+
         '<div id="traits_'+frog_id+'" class="trait_list">'+
           '<strong>Properties</strong><div id="owner_'+frog_id+'"style="float:right;">'+truncateAddress(owner)+'</div><div id="prop_'+frog_id+'" class="properties"></div>'+
         '</div>'+
