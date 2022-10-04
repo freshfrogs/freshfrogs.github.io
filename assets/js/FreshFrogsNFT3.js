152e6f9eb3d95930a488086e112606ca12358ff7
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-10-04 14:31:38 -0500

    saves

--- assets/js/FreshFrogsNFT3.js
+++ assets/js/FreshFrogsNFT3.js
@@ -253,16 +253,16 @@
       // Update UI
       Output(
         '<div>'+
-          '<div style="border-bottom: 1px solid white">'+
+          '<div class="terminalTop">'+
             '<b>Wallet Address</b>'+
             '<br>'+user_address+
           '</div>'+
           '<br>'+
-          '<div style="border-right: 1px solid white">'+
+          '<div class="terminalBottom">'+
             '<b>Total Supply</b>'+
             '<br>'+next_id+' / 4040'+
           '</div>'+
-          '<div>'+
+          '<div class="terminalBottom">'+
             '<b>Mint Limit</b>'+
             '<br>'+'Nine Tokens per Wallet'+
           '</div>'+
