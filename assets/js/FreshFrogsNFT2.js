7476c8d627124662a73d0412430af215d2ba0cf5
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-10-03 23:33:17 -0500

    Update FreshFrogsNFT2.js

--- assets/js/FreshFrogsNFT2.js
+++ assets/js/FreshFrogsNFT2.js
@@ -282,7 +282,7 @@
           '<button style="list-style: none; height: 40px; padding: 0; border-radius: 5px; border: 1px solid black; width: 270px; box-shadow: 3px 3px rgb(122 122 122 / 20%); margin: 16px; margin-left: auto; margin-right: auto; line-height: 1; text-align: center; vertical-align: middle;" class="frog_button">'+
             '<strong>FreshFrogsNFT Staking Vault</strong><br>'+user_tokens+' Total Frogs Staked!'+
           '</button>'+'<br>'+
-          '<hr style="background: black;">'+
+          //'<hr style="background: black;">'+
           '<div class="console_pre" id="console-pre"></div>');
       } else {
         Output(
@@ -290,7 +290,7 @@
             '<acc style="color: #333 !important;">[ '+truncateAddress(fetch_address)+' ]</acc><br>'+staker_tokens+' / '+user_tokens+' Frog(s) Staked '+' | '+staker_rewards+' $FLYZ 🡥'+
           '</button>'+'<br>'+
           //'<button class="frog_button" onclick="randomMorph()">Rando Morph</button>'+//'<button class="frog_button" onclick="">Custom Banner</button>'+'<button class="frog_button" onclick="">Staking Vault</button>'+
-          '<hr style="background: black;">'+
+          //'<hr style="background: black;">'+
           '<div class="console_pre" id="console-pre"></div>');
       }
 
