15f09107fecd4cfd68d122d21850d21ea7a10f23
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-10-05 01:29:40 -0500

    Update FreshFrogsNFT3.js

--- assets/js/FreshFrogsNFT3.js
+++ assets/js/FreshFrogsNFT3.js
@@ -513,6 +513,7 @@
     frog_token = document.createElement('div');
     frog_token.id = frog_name;
     frog_token.className = 'frog_token';
+    
     //frog_token.onclick = function() { display_token(frog_id); }
 
     // Element Inner HTML
