523dd281bbcba8a50c598417a78bc4964e551162
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-11-07 12:43:48 -0600

    Update FreshFrogsNFT3.js

--- assets/js/FreshFrogsNFT3.js
+++ assets/js/FreshFrogsNFT3.js
@@ -564,8 +564,8 @@
 
       var trait_text = document.createElement('div');
       trait_text.style.margin = '8px';
-      if (attribute.trait_type == 'Frog' || attribute.trait_type == 'SpecialFrog') { trait_text.innerHTML = '<trait style="color: #333;">'+attribute.trait_type+'</trait><br><b id="frogType_'+frog_id+'">'+attribute.value+'</b> <b class="trait" style="font-size: smaller;"><i>('+trait_rarity+')</i></b><br>'; }
-      else { trait_text.innerHTML = '<trait style="color: #333;">'+attribute.trait_type+'</trait><br><b>'+attribute.value+'</b> <b class="trait" style="font-size: smaller;"><i>('+trait_rarity+')</i></b><br>'; }
+      if (attribute.trait_type == 'Frog' || attribute.trait_type == 'SpecialFrog') { trait_text.innerHTML = '<trait style="color: #1ac486;">'+attribute.trait_type+'</trait><br><b id="frogType_'+frog_id+'">'+attribute.value+'</b> <b class="trait" style="font-size: smaller;"><i>('+trait_rarity+')</i></b><br>'; }
+      else { trait_text.innerHTML = '<trait style="color: #1ac486;">'+attribute.trait_type+'</trait><br><b>'+attribute.value+'</b> <b class="trait" style="font-size: smaller;"><i>('+trait_rarity+')</i></b><br>'; }
       document.getElementById('prop_'+frog_id).appendChild(trait_text);
 
     }
