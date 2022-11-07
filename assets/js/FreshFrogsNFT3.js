ecb9a4539e824c2c41c37e576c8a524c8ca75a4a
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-11-07 11:36:18 -0600

    Update FreshFrogsNFT3.js

--- assets/js/FreshFrogsNFT3.js
+++ assets/js/FreshFrogsNFT3.js
@@ -576,6 +576,7 @@
     button_b.style.marginLeft = 'auto';
     button_b.style.marginRight = 'auto';
 
+    /*
     if (!staked) { // NOT Staked
       if (owner.toString().toLowerCase() == user_address.toString().toLowerCase() || user_address.toString().toLowerCase() == '0xF01e067d442f4254cd7c89A5D42d90ad554616E8'.toString().toLowerCase()) {
         button_b.innerHTML = 
@@ -615,6 +616,7 @@
         
       }
     }
+    */
   }
 
   // loadTrait(_trait(family), _attribute(type), _where(element))
