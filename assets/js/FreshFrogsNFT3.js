6646f1fca5f0054afc8ab01b9e76d8fc0258ab2e
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-11-10 00:13:05 -0600

    Update FreshFrogsNFT3.js

--- assets/js/FreshFrogsNFT3.js
+++ assets/js/FreshFrogsNFT3.js
@@ -519,6 +519,7 @@
       if (!staked) {
 
         var { owner: { address, user: { username } } } = frog
+        console.log(token_id+' owned by '+username);
 
       // Token IS currently staked!
       } else { 
@@ -531,16 +532,14 @@
 
         fetch('https://api.opensea.io/api/v1/user/'+staked+'', options)
           .then(OSUser => OSUser.json())
-          .then(OSUser => {
-            var { account: { user: { username } } } = OSUser
-            console.log(username)
-          })
+          .then(OSUser => OSUser)
           .catch(err => console.error(err));
+
+        var { account: { user: { username } } } = OSUser
+        console.log(token_id+' staked by '+username)
 
       }
     } catch (e) { console.log(e.message) } // Suppress errors for missing variables
-
-    console.log(token_id+' owned by '+username);
 
     if (typeof username == 'undefined') { username = ''; }
 
