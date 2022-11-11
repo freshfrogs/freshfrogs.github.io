67b6611d698b10dbef9fd86ba8bd15040442a21b
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-11-11 01:18:51 -0600

    Update FreshFrogsNFT3.js

--- assets/js/FreshFrogsNFT3.js
+++ assets/js/FreshFrogsNFT3.js
@@ -587,12 +587,21 @@
               '<b>'+name+'</b> <text style="color: #1ac486;">'+opensea_username+'</text>'+'<text style="color: #1ac486; float: right;">'+rarity_rank+'</text>'+
             '</div>'+
             '<div id="prop_'+token_id+'" class="properties">'+
-              '<div style="margin: 8px; float: left;">'+
+              '<div style="margin: 8px; float: left; width: 100px;">'+
                 '<text>Time Staked</text>'+'<br>'+
                 '<text style="color: #1ac486;">'+''+' hours (Lvl '+''+')</text>'+
               '</div>'+
-              '<div style="margin: 8px; float: right;">'+
+              '<div style="margin: 8px; float: right; width: 100px;">'+
                 '<text>$FLYZ Earned</text>'+'<br>'+
+                '<text style="color: #1ac486;">110.69</text>'+
+              '</div>'+
+              '<br>'+
+              '<div style="margin: 8px; float: left; width: 100px;">'+
+                '<text>Staked Level</text>'+'<br>'+
+                '<text style="color: #1ac486;">'+''+' hours (Lvl '+''+')</text>'+
+              '</div>'+
+              '<div style="margin: 8px; float: right; width: 100px;">'+
+                '<text>Next Level</text>'+'<br>'+
                 '<text style="color: #1ac486;">110.69</text>'+
               '</div>'+
               '<div style="text-align: center;">'+
