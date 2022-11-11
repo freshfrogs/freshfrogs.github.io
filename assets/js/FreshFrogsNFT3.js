a9d41a1a44aae0aa5cef05a6ce3abe2b8295a272
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-11-10 22:33:31 -0600

    Update FreshFrogsNFT3.js

--- assets/js/FreshFrogsNFT3.js
+++ assets/js/FreshFrogsNFT3.js
@@ -529,7 +529,7 @@
       */
 
         /// test AGAIN
-        
+
     }
 
   /*
@@ -562,7 +562,7 @@
 
     console.log(token_id+' : '+opensea_username)
 
-    if (typeof opensea_username == 'undefined' || opensea_username == '' || typeof opensea_username == 'null' || opensea_username == 'null') {
+    if (typeof opensea_username == 'undefined' || opensea_username == '' || opensea_username == null) {
       console.log('username is null')
       opensea_username = truncateAddress(token_owner)
     }
