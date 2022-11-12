b63023840751f9d3d4d912f748bf0d8ade241ff6
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-11-11 20:05:11 -0600

    Update FreshFrogsNFT3.js

--- assets/js/FreshFrogsNFT3.js
+++ assets/js/FreshFrogsNFT3.js
@@ -369,7 +369,10 @@
             var { assets } = tokens
             assets.forEach((frog) => {
 
-              render_token(frog);
+              //render_token(frog);
+
+              var { token_id } = frog
+              stakedLeaderboard(token_id)
 
             })
           })
@@ -393,14 +396,14 @@
     }
 
     // Staked Leader Board
-    if (render_vault) {
+    //if (render_vault) {
       console.log(' -- Staked Leaderboard -- ');
       console.log(' Longest Streak: Frog #'+leaderboard_streak_token+' '+parseInt(leaderboard_streak/24)+' days');
       console.log(' Staked By: '+truncateAddress(leaderboard_streak_owner));
       console.log(' ');
       console.log(' Most Staked: '+leaderboard_totalStaked+' Frogs');
       console.log(' Staked By: '+truncateAddress(leaderboard_totalStaked_owner));
-    }
+    //}
     
   }
 
