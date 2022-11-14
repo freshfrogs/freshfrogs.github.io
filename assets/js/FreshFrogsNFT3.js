3693682e70c468b54f4c4f8cf86820fafa106263
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-11-13 21:11:16 -0600

    Update FreshFrogsNFT3.js

--- assets/js/FreshFrogsNFT3.js
+++ assets/js/FreshFrogsNFT3.js
@@ -690,7 +690,7 @@
     newAttribute = document.createElement("img");
     newAttribute.alt = attribute
 
-    if (attribute == 'closedEyes' || attribute == 'thirdEye' || attribute == 'mask' || attribute == 'smoking' || attribute == 'smokingCigar' || attribute == 'smokingPipe' || attribute == 'tongue' || attribute == 'circleShadesRed' || attribute == 'circleShadesPurple' || attribute == 'shades' || attribute == 'shadesPurple' || attribute == 'shadesThreeD' || attribute == 'shadesWhite' || attribute == 'circleNightVision') {
+    if (attribute == 'inversedEyes' || attribute == 'closedEyes' || attribute == 'thirdEye' || attribute == 'mask' || attribute == 'smoking' || attribute == 'smokingCigar' || attribute == 'smokingPipe' || attribute == 'tongue' || attribute == 'circleShadesRed' || attribute == 'circleShadesPurple' || attribute == 'shades' || attribute == 'shadesPurple' || attribute == 'shadesThreeD' || attribute == 'shadesWhite' || attribute == 'circleNightVision') {
       newAttribute.src = "https://freshfrogs.io/the-pond/"+trait+"/animations/"+attribute+"_animation.gif";
     } else {
       newAttribute.src = "https://freshfrogs.io/the-pond/"+trait+"/"+attribute+".png";
