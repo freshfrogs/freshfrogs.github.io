be80b4abcc157ed6ceb5dfb76d6e66e3f00b4a89
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-12-16 23:33:24 -0600

    d

--- script/frogs.js
+++ script/frogs.js
@@ -2,6 +2,9 @@
   // Variables
   var user_address, user_invites, user_keys, user_tokens, user_invite;
   var mint_price, mint_limit, mint_quantity, mint_total;
+  var staked_tokens, total_staked;
+  var controller, CONTROLLER, CONTROLLER_ADDRESS;
+  var CONTROLLER_ADDRESS = '0xCB1ee125CFf4051a10a55a09B10613876C4Ef199';
   var mint_price = mint_total = 0.01;
   var mint_quantity = 1;
   var next_id, traits_list, web3, f0;
@@ -24,9 +27,6 @@
   // fetch_user_tokens() | address
   async function fetch_user_tokens(fetch_address) {
     if (! fetch_address) { fetch_address = user_address; }
-
-    // No. tokens owned by fetch_address
-    let user_tokens = await collection.methods.balanceOf(fetch_address).call();
 
     document.getElementById('frogs').innerHTML = ''
 
@@ -99,32 +99,183 @@
       
     });
 
-    document.getElementById('quantity+').addEventListener("click", function(e) {
-
-      if (mint_quantity >= 10) { return; }
-      else {
-        mint_quantity = mint_quantity + 1;
-        mint_total = mint_price*mint_quantity;
-        document.getElementById('mint_quantity').innerHTML = mint_quantity
-        document.getElementById('button_left').innerHTML = '<strong>Mint</strong>'+mint_total.toFixed(3)+'Ξ'
-        document.getElementById('mintImage').src = '../frog/'+((next_id+mint_quantity)-1)+'.png'
-        document.getElementById('button_middle').innerHTML = '<strong>Frog</strong>'+((next_id+mint_quantity)-1)
-      }
-
-    })
-
-    document.getElementById('quantity-').addEventListener("click", function(e) {
-      if (mint_quantity <= 1) { return; }
-      else {
-        mint_quantity = mint_quantity - 1;
-        mint_total = mint_price*mint_quantity;
-        document.getElementById('mint_quantity').innerHTML = mint_quantity
-        document.getElementById('button_left').innerHTML = '<strong>Mint</strong>'+mint_total.toFixed(3)+'Ξ'
-        document.getElementById('mintImage').src = '../frog/'+((next_id+mint_quantity)-1)+'.png'
-        document.getElementById('button_middle').innerHTML = '<strong>Frog</strong>'+((next_id+mint_quantity)-1)
-      }
-
-    })
+    // Staking Contract ABI
+    const CONTROLLER_ABI =
+      [
+        {
+        "inputs": [],
+        "name": "claimRewards",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "uint256",
+            "name": "_tokenId",
+            "type": "uint256"
+            }
+        ],
+        "name": "stake",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "contract IERC721",
+            "name": "_nftCollection",
+            "type": "address"
+            },
+            {
+            "internalType": "contract IERC20",
+            "name": "_rewardsToken",
+            "type": "address"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "uint256",
+            "name": "_tokenId",
+            "type": "uint256"
+            }
+        ],
+        "name": "withdraw",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "address",
+            "name": "_staker",
+            "type": "address"
+            }
+        ],
+        "name": "availableRewards",
+        "outputs": [
+            {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "address",
+            "name": "_user",
+            "type": "address"
+            }
+        ],
+        "name": "getStakedTokens",
+        "outputs": [
+            {
+            "components": [
+                {
+                "internalType": "address",
+                "name": "staker",
+                "type": "address"
+                },
+                {
+                "internalType": "uint256",
+                "name": "tokenId",
+                "type": "uint256"
+                }
+            ],
+            "internalType": "struct FreshFrogsController.StakedToken[]",
+            "name": "",
+            "type": "tuple[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        },
+        {
+        "inputs": [],
+        "name": "nftCollection",
+        "outputs": [
+            {
+            "internalType": "contract IERC721",
+            "name": "",
+            "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        },
+        {
+        "inputs": [],
+        "name": "rewardsToken",
+        "outputs": [
+            {
+            "internalType": "contract IERC20",
+            "name": "",
+            "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+            }
+        ],
+        "name": "stakerAddress",
+        "outputs": [
+            {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+            }
+        ],
+        "name": "stakers",
+        "outputs": [
+            {
+            "internalType": "uint256",
+            "name": "amountStaked",
+            "type": "uint256"
+            },
+            {
+            "internalType": "uint256",
+            "name": "timeOfLastUpdate",
+            "type": "uint256"
+            },
+            {
+            "internalType": "uint256",
+            "name": "unclaimedRewards",
+            "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        }
+      ]
 
     // Connect WEB3, Factoria
     console.log('Connecting to Ethereum network...')
@@ -163,6 +314,12 @@
       // No. Tokens owned by fetch_address
       user_tokens = await collection.methods.balanceOf(user_address).call();
 
+      // No. Tokens staked by fetch_address
+      staked_tokens = await stakers(user_address, 'amountStaked')
+
+      // No. of total Frogs staked in contract
+      total_staked = await collection.methods.balanceOf(CONTROLLER_ADDRESS).call();
+
       // Collection Variables
       collection_name = await f0.api.name().call();
       collection_symbol = await f0.api.symbol().call();
@@ -196,6 +353,10 @@
               '<br>'+user_tokens+''+
             '</div>'+
             '<div class="terminalBottom">'+
+              '<owned class="displayUnit">Staked</owned>'+
+              '<br>'+staked_tokens+''+
+            '</div>'+
+            '<div class="terminalBottom">'+
               '<limit class="displayUnit">Limit</limit>'+
               '<br>'+mint_limit+''+
             '</div>'+
@@ -207,6 +368,33 @@
         '</div>'+
         '<div class="console_pre" id="console-pre"></div>'
       )
+
+      document.getElementById('quantity+').addEventListener("click", function(e) {
+
+        if (mint_quantity >= 10) { return; }
+        else {
+          mint_quantity = mint_quantity + 1;
+          mint_total = mint_price*mint_quantity;
+          document.getElementById('mint_quantity').innerHTML = mint_quantity
+          document.getElementById('button_left').innerHTML = '<strong>Mint</strong>'+mint_total.toFixed(3)+'Ξ'
+          document.getElementById('mintImage').src = '../frog/'+((next_id+mint_quantity)-1)+'.png'
+          document.getElementById('button_middle').innerHTML = '<strong>Frog</strong>'+((next_id+mint_quantity)-1)
+        }
+  
+      })
+  
+      document.getElementById('quantity-').addEventListener("click", function(e) {
+        if (mint_quantity <= 1) { return; }
+        else {
+          mint_quantity = mint_quantity - 1;
+          mint_total = mint_price*mint_quantity;
+          document.getElementById('mint_quantity').innerHTML = mint_quantity
+          document.getElementById('button_left').innerHTML = '<strong>Mint</strong>'+mint_total.toFixed(3)+'Ξ'
+          document.getElementById('mintImage').src = '../frog/'+((next_id+mint_quantity)-1)+'.png'
+          document.getElementById('button_middle').innerHTML = '<strong>Frog</strong>'+((next_id+mint_quantity)-1)
+        }
+  
+      })
 
       document.getElementById('button_left').addEventListener("click", async function(e) {
 
@@ -457,6 +645,122 @@
   function scroll_to(element) {
     console_pre = document.getElementById(element);
     console_pre.scrollIntoView({behavior: "smooth", block: "end", inline: "start"});
+  }
+
+  // Custom Front-End Functions
+
+  // Calculate total time a Frog has been staked (Hours)
+  async function timeStaked(tokenId) {
+
+    // Check Staked Status
+    web3 = new Web3(window.ethereum);
+    let staked = await stakerAddress(tokenId);
+
+    // Token is not currently staked
+    if (!staked) {
+
+      return 0.00
+
+    // Valid staked status
+    } else {
+      try {
+
+        // Loop blockchain transactions per parameters [NFT Transfer From: User ==> To: Staking Controller] & NFT is Currently Staked
+        let stakingEvents = await collection.getPastEvents('Transfer', { filter: {'to': CONTROLLER_ADDRESS, 'tokenId': tokenId}, fromBlock: 0, toBlock: 'latest'});
+        let mostRecentTxn = (stakingEvents.length) - 1;
+
+        // Fetch Block Number from Txn
+        let staked_block = parseInt(stakingEvents[mostRecentTxn].blockNumber);
+
+        // Fetch Timestamp for block txn
+        let staked_time = await web3.eth.getBlock(staked_block);
+        let staked_date = new Date(staked_time.timestamp*1000);
+
+        // Calculate Time Staked in Hours
+        let staked_duration = Date.now() - staked_date;
+        let staked_hours = Math.floor(staked_duration/1000/60/60);
+
+        //console.log('Frog #'+token_id+' Staked: '+staked_date.toUTCString()+' ('+staked_hours+' Hrs)');
+
+        // Return time staked in (Hours)
+        return staked_hours;
+
+      // Catch Errors, Return 0.00
+      } catch (e) { return 0.00; }
+    }
+  }
+
+  // <-----
+  // CALL()
+  // ----->
+
+  // availableRewards(_staker (address)) | return uint256
+  async function availableRewards(userAddress) {
+    return await controller.methods.availableRewards(userAddress).call();
+
+  }
+
+  // getStakedTokens(_user (address)) | return tuple[]
+  async function getStakedTokens(userAddress) {
+    return await controller.methods.getStakedTokens(userAddress).call();
+
+  }
+  
+  // nftCollection() | return address
+  async function nftCollection() {
+    return await controller.methods.nftCollection().call();
+
+  }
+
+  // rewardsToken() | return address
+  async function rewardsToken() {
+    return await controller.methods.rewardsToken().call();
+
+  }
+
+  /*
+
+    Retrieve staked token's true owner
+    stakerAddress(<input> (uint256)) | return staker's address or false
+
+  */
+
+  async function stakerAddress(tokenId) {
+
+    // Call function from controller contract
+    let stakerAddress = await controller.methods.stakerAddress(tokenId).call();
+
+    // Return staker's address
+    if (stakerAddress !== '0x0000000000000000000000000000000000000000') {
+      return stakerAddress;
+    }
+    
+    // Token is Not Currently Staked!
+    else { return false; }
+
+  }
+
+  // stakers(<input> (address), <input> (dataFetch)) | return ( amountStaked, timeOfLastUpdate, unclaimedRewards )
+  async function stakers(userAddress, _data) {
+    let stakers = await controller.methods.stakers(userAddress).call();
+
+    // Total Tokens Staked by User
+    if (_data == 'amountStaked') {
+      return stakers.amountStaked
+
+    // Time since Last Update from user
+    } else if (_data == 'timeOfLastUpdate') {
+      return stakers.timeOfLastUpdate
+
+    // Total unclaimed Rewards from User
+    } else if (_data == 'unclaimedRewards') {
+      return stakers.unclaimedRewards
+
+    // Invalid arguments
+    } else {
+      return
+
+    }
   }
 
 // Coded by NF7UOS
