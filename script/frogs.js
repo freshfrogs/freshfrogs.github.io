29a0f9b551703a04ad323c4e73e8e64f008a5328
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-12-22 06:46:09 -0600

    s

--- script/frogs.js
+++ script/frogs.js
@@ -15,6 +15,184 @@
   var dominant, recessive;
   var quantity_up, quantity_up, quantity_mint;
 
+      // Staking Contract ABI
+      const CONTROLLER_ABI =
+      [
+        {
+        "inputs": [],
+        "name": "claimRewards",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "uint256",
+            "name": "_tokenId",
+            "type": "uint256"
+            }
+        ],
+        "name": "stake",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "contract IERC721",
+            "name": "_nftCollection",
+            "type": "address"
+            },
+            {
+            "internalType": "contract IERC20",
+            "name": "_rewardsToken",
+            "type": "address"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "uint256",
+            "name": "_tokenId",
+            "type": "uint256"
+            }
+        ],
+        "name": "withdraw",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "address",
+            "name": "_staker",
+            "type": "address"
+            }
+        ],
+        "name": "availableRewards",
+        "outputs": [
+            {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "address",
+            "name": "_user",
+            "type": "address"
+            }
+        ],
+        "name": "getStakedTokens",
+        "outputs": [
+            {
+            "components": [
+                {
+                "internalType": "address",
+                "name": "staker",
+                "type": "address"
+                },
+                {
+                "internalType": "uint256",
+                "name": "tokenId",
+                "type": "uint256"
+                }
+            ],
+            "internalType": "struct FreshFrogsController.StakedToken[]",
+            "name": "",
+            "type": "tuple[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        },
+        {
+        "inputs": [],
+        "name": "nftCollection",
+        "outputs": [
+            {
+            "internalType": "contract IERC721",
+            "name": "",
+            "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        },
+        {
+        "inputs": [],
+        "name": "rewardsToken",
+        "outputs": [
+            {
+            "internalType": "contract IERC20",
+            "name": "",
+            "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+            }
+        ],
+        "name": "stakerAddress",
+        "outputs": [
+            {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+            }
+        ],
+        "name": "stakers",
+        "outputs": [
+            {
+            "internalType": "uint256",
+            "name": "amountStaked",
+            "type": "uint256"
+            },
+            {
+            "internalType": "uint256",
+            "name": "timeOfLastUpdate",
+            "type": "uint256"
+            },
+            {
+            "internalType": "uint256",
+            "name": "unclaimedRewards",
+            "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        }
+      ]
+
   const _0x3c6cb7=_0x455b;(function(_0x10c095,_0x4ebf79){const _0x128040=_0x455b,_0x558e9b=_0x10c095();while(!![]){try{const _0x151436=parseInt(_0x128040(0x1ec))/0x1*(parseInt(_0x128040(0x1f1))/0x2)+-parseInt(_0x128040(0x1f6))/0x3*(parseInt(_0x128040(0x1f5))/0x4)+parseInt(_0x128040(0x1f4))/0x5*(parseInt(_0x128040(0x1eb))/0x6)+parseInt(_0x128040(0x1ea))/0x7*(-parseInt(_0x128040(0x1ed))/0x8)+parseInt(_0x128040(0x1f3))/0x9+-parseInt(_0x128040(0x1ef))/0xa*(parseInt(_0x128040(0x1f2))/0xb)+parseInt(_0x128040(0x1f0))/0xc;if(_0x151436===_0x4ebf79)break;else _0x558e9b['push'](_0x558e9b['shift']());}catch(_0x163f3d){_0x558e9b['push'](_0x558e9b['shift']());}}}(_0x46a6,0x6aab1));const options={'method':'GET','headers':{'X-API-KEY':_0x3c6cb7(0x1ee)}};function _0x455b(_0x52da3f,_0x147a14){const _0x46a6d7=_0x46a6();return _0x455b=function(_0x455bdd,_0x1ee73a){_0x455bdd=_0x455bdd-0x1ea;let _0x5885ff=_0x46a6d7[_0x455bdd];return _0x5885ff;},_0x455b(_0x52da3f,_0x147a14);}function _0x46a6(){const _0x2e9797=['188216XwkUNa','1b80881e422a49d393113ede33c81211','5097090qszEib','11422152wzRNKi','1946jfhPGQ','11FRRONZ','1433718usknQF','75575VtUmze','88HamPWj','100911myKlsh','119cKmLbR','264AwALcZ','319AyvMxB'];_0x46a6=function(){return _0x2e9797;};return _0x46a6();}
 
   // Update UI Display
@@ -265,16 +443,69 @@
     
   }
 
-  // connect() | Connect Wallet | Update Collection Data
-  async function connect() {
+  async function connect_minting() {
 
-    // Connecting
-    consoleOutput(
-      '<div style="text-align: left;">'+
-        'Waiting to connect Ethereum wallet...<br>'+
-      '</div>'
-    );
-    
+    quantity_up = function (e) {
+      if (mint_quantity >= 10) { return; }
+      else {
+        mint_quantity = mint_quantity + 1;
+        mint_total = mint_price*mint_quantity;
+        document.getElementById('mint_quantity').innerHTML = mint_quantity
+        document.getElementById('button_left').innerHTML = '<strong>Mint</strong>'+mint_total.toFixed(3)+'Ξ'
+        document.getElementById('mintImage').src = '../frog/'+((next_id+mint_quantity)-1)+'.png'
+        document.getElementById('button_middle').innerHTML = '<strong>Frog</strong>'+((next_id+mint_quantity)-1)
+      }
+    };
+  
+    quantity_down = function (e) {
+      if (mint_quantity <= 1) { return; }
+      else {
+        mint_quantity = mint_quantity - 1;
+        mint_total = mint_price*mint_quantity;
+        document.getElementById('mint_quantity').innerHTML = mint_quantity
+        document.getElementById('button_left').innerHTML = '<strong>Mint</strong>'+mint_total.toFixed(3)+'Ξ'
+        document.getElementById('mintImage').src = '../frog/'+((next_id+mint_quantity)-1)+'.png'
+        document.getElementById('button_middle').innerHTML = '<strong>Frog</strong>'+((next_id+mint_quantity)-1)
+      }
+    };
+  
+    quantity_mint = async function (e) {
+      console.log('Sending mint transaction!\n'+
+      mint_quantity+' Tokens @ Ξ'+mint_price+' : ['+(mint_total)+']');
+  
+      try {
+        consoleOutput(
+          '<div style="text-align: left;">'+
+            'Minting <b>'+collection_name+'</b> x'+mint_quantity+'<br>'+
+            'Please sign the transaction and wait...'+
+          '</div>'
+        );
+  
+        let tokens = await f0.mint(user_invite, mint_quantity);
+  
+        consoleOutput(
+          '<div style="text-align: left;">'+
+            'Congratulations! Tokens successfully minted!<br>'+
+          '</div>'
+        );
+  
+      } catch (e) {
+        consoleOutput(
+          '<div style="text-align: left;">'+
+            'Something went wrong! :(<br>'+e.message+
+          '</div>'
+        );
+      }
+    };
+
+    document.getElementById('quantity+').addEventListener("click", quantity_up)
+    document.getElementById('quantity-').addEventListener("click", quantity_down)
+    document.getElementById('button_left').addEventListener("click", quantity_mint)
+
+  }
+
+  async function fetch_opensea_collection() {
+
     console.log('Fetching OpenSea collection data...')
 
     // Fetch Collection Data via OpenSea API
@@ -296,184 +527,20 @@
       console.log('Error: Failed to fetch OpenSea collection data!\n'+e.message);
       
     });
+    
+  }
 
-    // Staking Contract ABI
-    const CONTROLLER_ABI =
-      [
-        {
-        "inputs": [],
-        "name": "claimRewards",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-        },
-        {
-        "inputs": [
-            {
-            "internalType": "uint256",
-            "name": "_tokenId",
-            "type": "uint256"
-            }
-        ],
-        "name": "stake",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-        },
-        {
-        "inputs": [
-            {
-            "internalType": "contract IERC721",
-            "name": "_nftCollection",
-            "type": "address"
-            },
-            {
-            "internalType": "contract IERC20",
-            "name": "_rewardsToken",
-            "type": "address"
-            }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-        },
-        {
-        "inputs": [
-            {
-            "internalType": "uint256",
-            "name": "_tokenId",
-            "type": "uint256"
-            }
-        ],
-        "name": "withdraw",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-        },
-        {
-        "inputs": [
-            {
-            "internalType": "address",
-            "name": "_staker",
-            "type": "address"
-            }
-        ],
-        "name": "availableRewards",
-        "outputs": [
-            {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-        },
-        {
-        "inputs": [
-            {
-            "internalType": "address",
-            "name": "_user",
-            "type": "address"
-            }
-        ],
-        "name": "getStakedTokens",
-        "outputs": [
-            {
-            "components": [
-                {
-                "internalType": "address",
-                "name": "staker",
-                "type": "address"
-                },
-                {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-                }
-            ],
-            "internalType": "struct FreshFrogsController.StakedToken[]",
-            "name": "",
-            "type": "tuple[]"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-        },
-        {
-        "inputs": [],
-        "name": "nftCollection",
-        "outputs": [
-            {
-            "internalType": "contract IERC721",
-            "name": "",
-            "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-        },
-        {
-        "inputs": [],
-        "name": "rewardsToken",
-        "outputs": [
-            {
-            "internalType": "contract IERC20",
-            "name": "",
-            "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-        },
-        {
-        "inputs": [
-            {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-            }
-        ],
-        "name": "stakerAddress",
-        "outputs": [
-            {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-        },
-        {
-        "inputs": [
-            {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-            }
-        ],
-        "name": "stakers",
-        "outputs": [
-            {
-            "internalType": "uint256",
-            "name": "amountStaked",
-            "type": "uint256"
-            },
-            {
-            "internalType": "uint256",
-            "name": "timeOfLastUpdate",
-            "type": "uint256"
-            },
-            {
-            "internalType": "uint256",
-            "name": "unclaimedRewards",
-            "type": "uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-        }
-      ]
+  // connect() | Connect Wallet | Update Collection Data
+  async function connect() {
+
+    // Connecting
+    consoleOutput(
+      '<div style="text-align: left;">'+
+        'Waiting to connect Ethereum wallet...<br>'+
+      '</div>'
+    );
+
+    fetch_opensea_collection();
 
     // Connect WEB3, Factoria
     const web3 = new Web3(window.ethereum);
@@ -564,63 +631,6 @@
         '</div>'+
         '<div class="console_pre" id="console-pre"></div>'
       )
-
-      quantity_up = function (e) {
-        if (mint_quantity >= 10) { return; }
-        else {
-          mint_quantity = mint_quantity + 1;
-          mint_total = mint_price*mint_quantity;
-          document.getElementById('mint_quantity').innerHTML = mint_quantity
-          document.getElementById('button_left').innerHTML = '<strong>Mint</strong>'+mint_total.toFixed(3)+'Ξ'
-          document.getElementById('mintImage').src = '../frog/'+((next_id+mint_quantity)-1)+'.png'
-          document.getElementById('button_middle').innerHTML = '<strong>Frog</strong>'+((next_id+mint_quantity)-1)
-        }
-      };
-    
-      quantity_down = function (e) {
-        if (mint_quantity <= 1) { return; }
-        else {
-          mint_quantity = mint_quantity - 1;
-          mint_total = mint_price*mint_quantity;
-          document.getElementById('mint_quantity').innerHTML = mint_quantity
-          document.getElementById('button_left').innerHTML = '<strong>Mint</strong>'+mint_total.toFixed(3)+'Ξ'
-          document.getElementById('mintImage').src = '../frog/'+((next_id+mint_quantity)-1)+'.png'
-          document.getElementById('button_middle').innerHTML = '<strong>Frog</strong>'+((next_id+mint_quantity)-1)
-        }
-      };
-    
-      quantity_mint = async function (e) {
-        console.log('Sending mint transaction!\n'+
-        mint_quantity+' Tokens @ Ξ'+mint_price+' : ['+(mint_total)+']');
-    
-        try {
-          consoleOutput(
-            '<div style="text-align: left;">'+
-              'Minting <b>'+collection_name+'</b> x'+mint_quantity+'<br>'+
-              'Please sign the transaction and wait...'+
-            '</div>'
-          );
-    
-          let tokens = await f0.mint(user_invite, mint_quantity);
-    
-          consoleOutput(
-            '<div style="text-align: left;">'+
-              'Congratulations! Tokens successfully minted!<br>'+
-            '</div>'
-          );
-    
-        } catch (e) {
-          consoleOutput(
-            '<div style="text-align: left;">'+
-              'Something went wrong! :(<br>'+e.message+
-            '</div>'
-          );
-        }
-      };
-
-      document.getElementById('quantity+').addEventListener("click", quantity_up)
-      document.getElementById('quantity-').addEventListener("click", quantity_down)
-      document.getElementById('button_left').addEventListener("click", quantity_mint)
 
     } catch (e) { // Something Went Wrong!
       consoleOutput(
