43501358ee5011487437e708868390fe9e9ac476
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2023-07-02 22:43:26 -0500

    Update ffstatic.js

--- source/ffstatic.js
+++ source/ffstatic.js
@@ -10,7 +10,7 @@ async function fetch_collection(order) {
     console.log(SOURCE_PATH)
 
     var arr = [];
-    while(arr.length < 207){
+    while(arr.length < 3){
         var r = Math.floor(Math.random() * 2222) + 1;
         if(arr.indexOf(r) === -1) arr.push(r);
     }
