39b56371c49297fc49a2376615923d697cd195f1
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2023-07-07 19:16:28 -0500

    Update ffstatic.js

--- source/ffstatic.js
+++ source/ffstatic.js
@@ -12,7 +12,7 @@
 
         var arr = [];
 
-        while(arr.length < 207){
+        while(arr.length < 1){
             var r = Math.floor(Math.random() * 2222) + 1;
             if(arr.indexOf(r) === -1) arr.push(r);
         }
