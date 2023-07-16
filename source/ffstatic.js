b2eada0a48b0191e3bca8468295e4657972f7987
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2023-07-16 15:31:26 -0500

    Update ffstatic.js

--- source/ffstatic.js
+++ source/ffstatic.js
@@ -8,22 +8,22 @@
     // Fetch Collection
     async function fetch_collection() {
 
-        toadA = toadB = toadC = '';
+    //    toadA = toadB = toadC = '';
 
         var arr = [];
 
-        while(arr.length < 2){
+        while(arr.length < 207){
             var r = Math.floor(Math.random() * 2222) + 1;
             if(arr.indexOf(r) === -1) arr.push(r);
         }
 
         for (let i = 0; i < arr.length; i++) {
 
-            if (toadA == '') {
-                toadA = arr[i]
-            } else if (toadB == '') {
-                toadB = arr[i]
-            }
+        //    if (toadA == '') {
+        //        toadA = arr[i]
+        //    } else if (toadB == '') {
+        //        toadB = arr[i]
+        //    }
 
             await display_token(arr[i])
 
@@ -31,7 +31,7 @@
 
         // Third Object
         
-        //
+        /*
 
         // Random background
         var r2 = Math.floor(Math.random() * 2222) + 1;
@@ -52,6 +52,8 @@
         token_doc.appendChild(token_element);
 
         morphFrogs(toadA, toadB, 'cont_morphResult');
+
+        */
         
             
     }
