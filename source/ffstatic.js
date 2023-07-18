50266f8dbc07a88bd1b2b3457bee04a72b13a468
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2023-07-17 22:11:37 -0500

    Toad Morph Subset v3

--- source/ffstatic.js
+++ source/ffstatic.js
@@ -220,7 +220,7 @@
         if (charlieMetadata['Toad'] !== '') { loadTrait('Toad', charlieMetadata['Toad'], build_loc); }
         
         // Bravo (OVERLAY)
-        if (charlieMetadata['ToadSubset'] !== '') { loadTrait('Toad/subset/v2', charlieMetadata['ToadSubset'], build_loc); }
+        if (charlieMetadata['ToadSubset'] !== '') { loadTrait('Toad/subset/v3', charlieMetadata['ToadSubset'], build_loc); }
 
         // TRAIT(S)
         if (bravoMetadata['Trait'] !== '') { loadTrait('Trait', bravoMetadata['Trait'], build_loc); }
