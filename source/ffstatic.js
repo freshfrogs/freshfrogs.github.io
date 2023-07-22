554f650336be41d0d84871008ae462322b69f7ad
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2023-07-21 23:18:02 -0500

    Update ffstatic.js

--- source/ffstatic.js
+++ source/ffstatic.js
@@ -206,7 +206,7 @@
 
     async function stake(tokenId) {
 
-        if (Number.isInteger(value) == false || tokenId > 4040 || tokenId == 0) { return 'TXN FAILED:\n Invalid token ID!'; }
+        if (Number.isInteger(tokenId) == false || tokenId > 4040 || tokenId == 0) { return 'TXN FAILED:\n Invalid token ID!'; }
 
         // Check Ownership / Approval Status
         let owner = await collection.methods.ownerOf(tokenId).call();
