9bb919c711cd096ad19d1cfbca4797d02e69543d
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2023-07-25 13:15:29 -0500

    Update ffstatic.js

--- source/ffstatic.js
+++ source/ffstatic.js
@@ -1196,8 +1196,8 @@
         // Use Functions?
         button_elements = 
             '<div style="text-align: center;">'+
-                '<button class="unstake_button" onclick="initiate_withdraw('+token_id+')"><span class="material-symbols-outlined">change_circle</span>Un-stake</button>'+
-                '<a class="" target="_blank" href="https://freshfrogs.github.io/frog/json/'+token_id+'.json"><button class="unstake_button"><span class="material-symbols-outlined">open_in_new</span>View Metadata</button></a>'
+                '<button class="unstake_button" onclick="initiate_withdraw('+token_id+')">Un-stake</button>'+
+                '<a class="" target="_blank" href="https://freshfrogs.github.io/frog/json/'+token_id+'.json"><button class="unstake_button">View Metadata</button></a>'
             '</div>';
     }
 
