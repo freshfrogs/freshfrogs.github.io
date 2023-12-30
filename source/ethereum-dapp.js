074c323a10da2a56dd84672c1b9de5e5c959fdba
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2023-12-29 21:34:53 -0600

    Update ethereum-dapp.js

--- source/ethereum-dapp.js
+++ source/ethereum-dapp.js
@@ -532,6 +532,21 @@ async function send_write_function(contract_method) {
     }
 }
 
+// Numbers to roman numerals
+function romanize (num) {
+    if (isNaN(num))
+        return NaN;
+    var digits = String(+num).split(""),
+        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+                "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+                "","I","II","III","IV","V","VI","VII","VIII","IX"],
+        roman = "",
+        i = 3;
+    while (i--)
+        roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+    return Array(+digits.join("") + 1).join("M") + roman;
+}
+
 // Shorten ethereum wallet address
 function truncateAddress(address) {
     if (!address) { return ""; }
