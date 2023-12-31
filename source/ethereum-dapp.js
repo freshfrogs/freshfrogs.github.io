ba48af59af801326f3f9df7f7fbfa256f94e14e6
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2023-12-31 15:47:05 -0600

    Update ethereum-dapp.js

--- source/ethereum-dapp.js
+++ source/ethereum-dapp.js
@@ -331,7 +331,7 @@ async function initiate_stake(token_id) {
     if (!approved) { alert('Before you can begin staking the contract must be given permission to acess your Frogs. See "🌱 Stake & Earn!" to learn more.'); }
 
     // Passed all requisites. Request user to confirm token ID
-    var input = prompt("PLEASE READ: \nWhile tokens are staked, you will not be able to sell them on secondary market places. To do this you will have to un-stake directly from this site. Once a token is un-staked it's staking level will reset to zero!\n"+"\nConfirm the ID of the token you would like to stake:\nToken ID: ");
+    var input = prompt("PLEASE READ: \nWhile tokens are staked, you will not be able to sell them on secondary market places. To do this you will have to un-stake directly from this site. Once a token is un-staked it's staking level will reset to zero!\n"+"\nConfirm the ID of the token you would like to stake.\nToken ID: "+token_id);
     var input_id = parseInt(input)
     if (input !== null) {
         if (input_id !== token_id) {
@@ -404,7 +404,7 @@ async function initiate_withdraw(token_id) {
     if (!approved) { alert('TRANSACTION FAILED:\n Staking contract is missing approval!'); }
 
     // Passed all requisites. Request user to confirm token ID
-    var input = prompt("PLEASE READ: \nUn-staking (withdrawing) will return the token to your wallet. Staking level will be reset to zero!\n"+"\nConfirm the ID of the token you would like to withdraw:\nToken ID: ");
+    var input = prompt("PLEASE READ: \nUn-staking (withdrawing) will return the token to your wallet. Staking level will be reset to zero!\n"+"\nConfirm the ID of the token you would like to withdraw.\nToken ID: "+token_id);
     var input_id = parseInt(input)
     if (input !== null) {
         if (input_id !== token_id) {
