9782195b848d84d05b43e9e0d885343de2a27edf
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2024-01-01 01:20:24 -0600

    eth_usd price per time

--- source/ethereum-dapp.js
+++ source/ethereum-dapp.js
@@ -102,10 +102,12 @@ const options = {
     }
   };
 
-async function fetch_eth_usd() {
+async function fetch_eth_usd(block) {
+    if(! block){ block = ''; }
+    else { block = '&to_block='+block}
 
     console.log('Fetching ETH/USD...')
-    fetch('https://deep-index.moralis.io/api/v2.2/erc20/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/price?chain=eth&include=percent_change', options)
+    fetch('https://deep-index.moralis.io/api/v2.2/erc20/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/price?chain=eth&include=percent_change'+block, options)
     .then((results) => results.json())
     .then((results) => { return Number(results.usdPrice); })
 
@@ -116,6 +118,8 @@ async function fetch_recent_sales(ammount) {
     fetch('https://deep-index.moralis.io/api/v2.2/nft/'+COLLECTION_ADDRESS+'/trades?chain=eth&marketplace=opensea', options)
     .then((tokens) => tokens.json())
     .then((tokens) => {
+
+        console.log(tokens);
 
         var assets = tokens.result
         var shuffled, asset_tokens;
