eacca66b54081bde2248975e2ea212c1fd42af3c
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2024-01-01 00:51:56 -0600

    Update ethereum-dapp.js

--- source/ethereum-dapp.js
+++ source/ethereum-dapp.js
@@ -740,8 +740,8 @@ async function render_recently_sold(token) {
 
     var { token_ids, seller_address, buyer_address, price, block_timestamp } = token
     var token_id = token_ids[0]
-    var sale_price = price / 1000000000000000000;
-    var sale_price_usd = sale_price * eth_usd;
+    var sale_price = Number(price / 1000000000000000000);
+    var sale_price_usd = (sale_price * eth_usd);
     var timestamp = block_timestamp.substring(0, 10);
     var location = 'frogs'
     var image_link = SOURCE_PATH+'/frog/'+token_id+'.png'
