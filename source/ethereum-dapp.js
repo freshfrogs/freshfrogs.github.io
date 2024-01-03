b5326cc510731c3cb9dfd04a0633e32d2f90990f
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2024-01-03 09:31:46 -0600

    Update ethereum-dapp.js

--- source/ethereum-dapp.js
+++ source/ethereum-dapp.js
@@ -213,13 +213,11 @@ async function fetch_nft_sales_data(limit, next_string) {
         if (next !== null && next !== '' && next !== 'undefined') {
             break_element = document.createElement('br')
             document.getElementById('frogs').appendChild(break_element)
-
             loadMore = document.createElement('button')
             loadMore.id = 'loadMore'
             loadMore.className = 'connectButton'
             loadMore.onclick = async function(){ document.getElementById('loadMore').remove(); await fetch_nft_sales_data('100', next); }
             loadMore.innerHTML = '🔰 Secondary Sales'
-
             document.getElementById('frogs').appendChild(loadMore)
         } else { return }
     })
