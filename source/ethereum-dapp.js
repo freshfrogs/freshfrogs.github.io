1c9fdb4bfe626fcedda532667f714d605738fa6d
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2024-01-03 20:31:34 -0600

    Update ethereum-dapp.js

--- source/ethereum-dapp.js
+++ source/ethereum-dapp.js
@@ -108,7 +108,7 @@ const CONTROLLER_ADDRESS = '0xCB1ee125CFf4051a10a55a09B10613876C4Ef199';
 const options = {method: 'GET', headers: {accept: '*/*', 'x-api-key': '3105c552-60b6-5252-bca7-291c724a54bf'}};
 async function fetch_token_sales(limit, next_string) {
     if (! limit) { limit = '5'; }
-    if (! next_string) { next = ''; } else { next = '&continuation'+next_string; console.log(next) }
+    if (! next_string) { next = ''; } else { next = '&continuation='+next_string; console.log(next) }
     console.log('Fetching recent sales data..')
     fetch('https://api.reservoir.tools/sales/v5?collection=0xBE4Bef8735107db540De269FF82c7dE9ef68C51b&limit='+limit+next+'', options)
     .then((data) => data.json())
