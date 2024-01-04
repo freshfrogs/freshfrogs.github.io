875b8576f48c5029a74578958460acc27a4788da
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2024-01-03 20:51:31 -0600

    Update ethereum-dapp.js

--- source/ethereum-dapp.js
+++ source/ethereum-dapp.js
@@ -136,7 +136,7 @@ async function render_token_sales(sales) {
 
         var { createdAt, from, to, token: { tokenId }, price: { amount: { decimal, usd } } } = token
         var sale_date = createdAt.substring(0, 10);
-        if (from == '0x0000000000000000000000000000000000000000000000000000000000000000') { var txn_string = 'sale'; }
+        if (from == '0x0000000000000000000000000000000000000000000000000000000000000000') { var txn_string = 'mint'; }
         else if (from !== '0x0000000000000000000000000000000000000000000000000000000000000000') { var txn_string = 'sale'; }
 
         // Render token information and data
