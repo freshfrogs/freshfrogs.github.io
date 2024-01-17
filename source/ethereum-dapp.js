3ff1b6fa793e728a118045a43791ee09df8f7588
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2024-01-17 12:20:50 -0600

    Update ethereum-dapp.js

--- source/ethereum-dapp.js
+++ source/ethereum-dapp.js
@@ -265,6 +265,7 @@ async function sales_load_button(contract, limit, next_string) {
         loadMore.onclick = async function(){ document.getElementById('loadMore').remove(); document.getElementById('tempBreak').remove(); await fetch_token_sales(contract, '100', next_string); }
         loadMore.innerHTML = '🔰 Load More'
         loadMore.style.width = '12%'
+        loadMore.style.minWidth = '120px'
         document.getElementById('frogs').appendChild(loadMore)
     } else { return; }
 }
