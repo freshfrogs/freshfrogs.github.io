650be9c3c556a390820005e228d6166a039d3065
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2025-01-04 14:38:44 -0600

    Update ethereum-dapp.js

--- source/ethereum-dapp.js
+++ source/ethereum-dapp.js
@@ -232,8 +232,8 @@ async function render_token_sales(contract, sales) {
             '<div class="infobox_right">'+
                 '<text style="color: #1a202c; font-weight: bold;">Owner</text>'+'<br>'+
                 '<text style="color: teal; font-weight: bold;">'+truncateAddress(to)+'</text>'+
-            '</div>'+
-            '<br>'+
+            '</div>';
+            /*'<br>'+
             '<div class="infobox_left">'+
                 '<text style="color: #1a202c; font-weight: bold;">Seller</text>'+'<br>'+
                 '<text style="color: teal; font-weight: bold;">'+from+'</text>'+
@@ -245,7 +245,7 @@ async function render_token_sales(contract, sales) {
             '<div style="text-align: center;">'+
                 '<br><a href="https://etherscan.io/tx/'+txHash+'" target="_blank"><button class="etherscan_button">Etherscan</button></a>'+
                 '<a href="https://opensea.io/assets/ethereum/0xbe4bef8735107db540de269ff82c7de9ef68c51b/'+tokenId+'" target="_blank"><button class="opensea_button">OpenSea</button></a>'+
-            '</div>';
+            '</div>';*/
         //if (txn_string == 'sale') {
             await build_token(html_elements, tokenId, tokenId+':'+createdAt, txn_string, txHash);
         //}
