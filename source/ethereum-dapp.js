a345f517ff834f1b426a9f58c7b5663e9fb63f91
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2025-01-05 01:43:52 -0600

    Update

--- source/ethereum-dapp.js
+++ source/ethereum-dapp.js
@@ -242,7 +242,6 @@ async function render_token_sales(contract, sales) {
             '<div id="buttonsPanel_'+tokenId+'" class="card_buttonbox">'+
                 '<a href="https://etherscan.io/nft/0xbe4bef8735107db540de269ff82c7de9ef68c51b/'+tokenId+'" target="_blank"><button class="etherscan_button">Etherscan</button></a>'+
                 '<a href="https://opensea.io/assets/ethereum/0xbe4bef8735107db540de269ff82c7de9ef68c51b/'+tokenId+'" target="_blank"><button class="opensea_button">Opensea</button></a>'+
-                button_element+
             '</div>';
 
         await build_token(html_elements, tokenId, tokenId+':'+createdAt, txn_string, txHash);
