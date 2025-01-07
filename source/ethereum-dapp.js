06c617d5baa5928949249ee8daca37ce44401ccc
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2025-01-06 23:42:38 -0600

    Update ethereum-dapp.js

--- source/ethereum-dapp.js
+++ source/ethereum-dapp.js
@@ -847,9 +847,6 @@ async function connect_user() {
             web3 = new Web3(window.ethereum);
 
             user_address = await web3.currentProvider.selectedAddress;
-            if (user_address.toLowerCase() == '0xF01e067d442f4254cd7c89A5D42d90ad554616E8'.toLowerCase()) {
-                user_address = '0x8Fe45D16694C0C780f4c3aAa6fCa2DDB6E252B25'
-            }
 
             f0 = new F0();
             console.log('f0 connection');
