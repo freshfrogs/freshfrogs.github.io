0a352d9ec5a4c9eebd67e259426203b27dbc4dad
FreshFrogsNFT <97641869+freshfrogs@users.noreply.github.com>
2025-01-07 14:59:30 -0600

    Update ethereum-dapp.js

--- source/ethereum-dapp.js
+++ source/ethereum-dapp.js
@@ -881,7 +881,7 @@ async function build_token(html_elements, token_id, element_id, txn, txn_hash) {
     let metadata = await (await fetch(SOURCE_PATH+'/frog/json/'+token_id+'.json')).json();
     for (let i = 0; i < metadata.attributes.length; i++) {
         let attribute = metadata.attributes[i]
-        rarityRanking = rarityScores[attribute.value] + rarityRanking
+        rarityRanking = Number(rarityScores[attribute.value]) + Number(rarityRanking)
         console.log(attribute.value+': '+rarityScores[attribute.value])
         /* if (attribute.trait_type == 'SpecialFrog' && attribute.value == 'peace') {
 
