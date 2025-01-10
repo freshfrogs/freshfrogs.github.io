e2c9117c1d3360e26cd94eb39698a7634bbaaeef
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2025-01-10 12:58:01 -0600

    Update rarityrankings.js

--- source/rarityrankings.js
+++ source/rarityrankings.js
@@ -51,7 +51,7 @@ async function rank_tokens() {
             if (attribute == 'natural' && trait_type == 'Trait') {
                 if (frog_type == 'redEyedTreeFrog' || frog_type == 'lightBrownTreeFrog' || frog_type == 'brownTreeFrog' || frog_type == 'goldenDartFrog' || frog_type == 'unknown' || frog_type == 'grayTreeFrog' || frog_type == 'stawberryDartFrog' || frog_type == 'blueDartFrog' || frog_type == 'splendidLeafFrog') {
                     
-                    console.log('Natural Rarity: '+rarity_trait_rankings[frog_type+'_natural'])
+                    console.log('Natural Rarity | '+rarity_trait_rankings[frog_type+'_natural'])
                     var rarity_raw = parseInt(rarity_token_rankings[i].rarity) + 1/(parseInt(rarity_trait_rankings[frog_type+'_natural'])/4040)
                     rarity_token_rankings[i].rarity = parseInt(rarity_raw);
 
