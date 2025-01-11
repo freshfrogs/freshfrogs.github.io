e91b0a3e649e1f866ee59bb1d8f77961b6f4d17d
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2025-01-11 01:35:06 -0600

    Update rarityrankings.js

--- source/rarityrankings.js
+++ source/rarityrankings.js
@@ -236,7 +236,7 @@ var rarity_trait_rankings = {
         'tongueSpiderRed': 70
     }
 
-    
+    /*
     var freshfrogs_rarity_rankings = 
     [
         {
@@ -20441,4 +20441,5 @@ var rarity_trait_rankings = {
         },
         null
     ]
+    */
     
