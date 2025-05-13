4fa9521e89d121bf685703a8bdc1fe56913d2b5e
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2025-05-12 20:37:02 -0500

    Update ethereum-dapp.js

--- assets/ethereum-dapp.js
+++ assets/ethereum-dapp.js
@@ -769,7 +769,7 @@ async function build_token(html_elements, token_id, element_id, txn, txn_hash, l
     token_element.innerHTML = 
 
          
-        '<div id="div_'+element_id+'" class="renderLeft">'+
+        '<div id="div_'+element_id+'" class="renderLeft"> style="background-image: url('+image_link+'); background-size: 512px 512px;">'+
             '<div class="innerLeft">'+
                 '<div class="display_token_img_cont" id="cont_'+element_id+'"'+
                 '</div>'+
