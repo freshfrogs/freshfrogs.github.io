b94c58581985492e5a7186e356d0833e1f8ef909
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2025-09-18 23:52:22 -0500

    Cleanup

---
+++ assets/js/config.js
@@ -0,0 +1,11 @@
+// Global config/constants
+window.FF_CFG = {
+  SOURCE_PATH: "https://freshfrogs.github.io",
+  SUPPLY: 4040,
+  COLLECTION_ADDRESS: "0xBE4Bef8735107db540De269FF82c7dE9ef68C51b",
+  CONTROLLER_ADDRESS: "0xCB1ee125CFf4051a10a55a09B10613876C4Ef199", // staking controller
+  JSON_PATH: "assets/freshfrogs_rarity_rankings.json",
+  PAGE_SIZE: 5,
+  // Set your Reservoir API key at runtime (or here)
+  FROG_API_KEY: (window.frog_api || "YOUR_RESERVOIR_API_KEY_HERE")
+};
