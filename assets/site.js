d9b8c7ec8b5c50444457f0e7e49acfb08c722a3b
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2025-11-18 18:05:33 -0600

    Update site.js

--- assets/site.js
+++ assets/site.js
@@ -13,7 +13,7 @@ const FF_ALCHEMY_CORE_BASE  = `https://eth-mainnet.g.alchemy.com/v2/${FF_ALCHEMY
 
 // 'sales' = use getNFTSales
 // 'mints' = use alchemy_getAssetTransfers (ERC721 mints)
-const FF_ACTIVITY_MODE      = 'sales'; // change to 'mints' if you want recent mints instead
+const FF_ACTIVITY_MODE      = 'mints'; // change to 'mints' if you want recent mints instead
 
 // ------------------------
 // Public entrypoint
