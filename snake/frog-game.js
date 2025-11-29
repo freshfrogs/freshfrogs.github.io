3ac81cd1f4f1aaf5db88dd2764436f3b88ea52dd
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2025-11-29 16:09:41 -0600

    Update frog-game.js

--- snake/frog-game.js
+++ snake/frog-game.js
@@ -199,7 +199,7 @@
   let nextPermanentChoiceTime = 60;
 
   // every 180 seconds we pause for an EPIC upgrade
-  let nextEpicChoiceTime = 10;
+  let nextEpicChoiceTime = 180;
 
   // 10-minute legendary choice
   const LEGENDARY_EVENT_TIME = 600; // 10 minutes
