ecf6a319c56d2a4b36756053b4b99896d391e5af
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2025-11-30 03:02:47 -0600

    Update frog-game.js

--- snake/frog-game.js
+++ snake/frog-game.js
@@ -111,7 +111,7 @@
   const MAX_FROG_JUMP_FACTOR          = 3.0;
   const MAX_BUFF_DURATION_FACTOR      = 3.5;
   const MIN_ORB_SPAWN_INTERVAL_FACTOR = 0.40;
-  const MAX_DEATHRATTLE_CHANCE        = 0.40;
+  const MAX_DEATHRATTLE_CHANCE        = 0.45;
   const MAX_ORB_COLLECTOR_TOTAL       = 1.0;
   const SNAKE_SHED_SPEEDUP = 1.27;
 
