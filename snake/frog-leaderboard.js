9623d9efaef7ad2d08bb9c8f919b2bec457c45bb
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2025-11-30 23:33:47 -0600

    Revert "Update frog-leaderboard.js"
    This reverts commit 79ceada985378be6ae861505f0b6592aa5900a1d.

--- snake/frog-leaderboard.js
+++ snake/frog-leaderboard.js
@@ -506,7 +506,7 @@
               ? s.frogDeathRattleChance
               : null);
   
-      console.log(`
+      statsBox.innerHTML = `
         <div style="font-weight:bold; margin-bottom:4px;">Run stats</div>
         <div>Deathrattle chance: ${fmtPct(deathrattleChance)}</div>
         <div>Frog speed factor: ${fmtMult(s.frogSpeedFactor)}</div>
@@ -514,7 +514,7 @@
         <div>Buff duration: ${fmtMult(s.buffDurationFactor)}</div>
         <div>Orb spawn interval factor: ${fmtMult(s.orbSpawnIntervalFactor)}</div>
         <div>Total frogs spawned: ${fmtInt(s.totalFrogsSpawned)}</div>
-      `);
+      `;
   
       scoreboardOverlayInner.appendChild(statsBox);
     }
