11f96ba9d9c0afd872ce3f2fe9ca88f4abbed81c
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2025-12-02 17:23:25 -0600

    Update frog-game.js

--- snake/frog-game.js
+++ snake/frog-game.js
@@ -2489,8 +2489,7 @@ function ensureHowToOverlay() {
     "Avoid the snake and stay alive as long as possible!",
     "Collect orbs to gain buffs and upgrades.",
     "Beat the high score to get on the leaderboard.",
-    "Control frogs with your mouse.",
-    "Best played in browser at <span style='color: lightgreen'><a href='https://freshfrogs.github.io/snake'>freshfrogs.github.io</a></span>"
+    "Control frogs with your mouse."
   ].forEach(text => {
     const li = document.createElement("li");
     li.textContent = text;
